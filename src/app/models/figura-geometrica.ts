6483c343a7b5ca79399e376ba90fe58a4fdbbc9c
Magnet-Sys <80355938+Magnet-Sys@users.noreply.github.com>
2024-12-22 18:23:36 -0300

    Comments are added to the GeometricFigure class

--- src/app/models/figura-geometrica.ts
+++ src/app/models/figura-geometrica.ts
@@ -1,9 +1,15 @@
+/**
+ * Clase abstracta que representa una figura geométrica.
+ * Proporciona una base para definir diferentes tipos de figuras geométricas.
+ */
 export abstract class FiguraGeometrica {
   nombre: string;
 
+  // Constructor de la clase FiguraGeometrica.
   constructor(nombre: string) {
+    // Nombre de la figura geométrica
     this.nombre = nombre;
   }
-
+  // Método abstracto para calcular el perímetro de la figura.
   abstract calcularPerimetro(): number;
 }
