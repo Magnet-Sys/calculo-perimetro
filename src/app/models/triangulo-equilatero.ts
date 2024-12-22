7c31bba8b2a4f8233af4097a94ab890d2753aa00
Magnet-Sys <80355938+Magnet-Sys@users.noreply.github.com>
2024-12-22 18:27:47 -0300

    Comments added to the Triangle class

--- src/app/models/triangulo-equilatero.ts
+++ src/app/models/triangulo-equilatero.ts
@@ -1,14 +1,19 @@
 import { FiguraGeometrica } from './figura-geometrica';
 
 export class TrianguloEquilatero extends FiguraGeometrica {
+  // Propiedad privada para almacenar la longitud del lado del triángulo
   private ladoA: number;
 
+  // Constructor de la clase Triangulo
   constructor(ladoA: number) {
+    // Llama al constructor de la clase padre (FiguraGeometrica) con el nombre de la figura
     super('Triángulo Equilátero');
+    // Inicializa la propiedad ladoA con el valor proporcionado.
     this.ladoA = ladoA;
   }
 
   calcularPerimetro(): number {
+    // Calcula el perímetro multiplicando la longitud del lado por 3.
     return 3 * this.ladoA;
   }
 }
